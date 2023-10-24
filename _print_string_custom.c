97bafd5cc1661259d9d5b5af38f9cf42ba900eb2
Merna70 <HanaaHussein414@gmail.com>
2023-10-24 16:24:32 -0700

    S

---
+++ _print_string_custom.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_str - chech string inserted
+ * @str:the string that we will print
+ * Return: number of charachters of this string
+ **/
+
+int _print_str(char *str)
+{
+	int x = 0
+
+	while (*str)
+	{
+		_print_char((int)*str);
+		x++
+		str++;
+	}
+
+	return (x);
+}
