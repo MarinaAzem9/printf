d64351e7d8f5298c228197a402ca3fc207e0a86f
Merna70 <HanaaHussein414@gmail.com>
2023-10-24 18:06:47 -0700

    P

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _print_int_demical(int basetodive);
+int _print_str(char *str);
+int _print_format_custom(char frmstp, va_list args);
+int _print_character(int ch);
+int _printf(const char word, ...);	
+
+#endif
