70da86e2dc9d84d757849d6ab4cc6c21aca1573b
Merna70 <HanaaHussein414@gmail.com>
2023-10-25 13:20:04 -0700

    main

--- main_0.c
+++ main_0.c
@@ -3,22 +3,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: 0 on success, error code otherwise
+ * main Main fun
+ * Return 0 if success 
  */
-int main(void)
+int main (void)
 {
 	int len, len2;
-
 	len = _printf("%d", 1024);
 	len2 = printf("%d", 1024);
+
 	fflush(stdout);
-	if (len != len2)
+	if(len != len2)
 	{
 		printf("Lengths differ.\n");
 		fflush(stdout);
-		return (1);
+		return(1);
 	}
-	return (0);
+	return(0);
 }
